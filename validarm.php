76effa8ca60feddc84c04e6fa32c7e8a3925c376
AHUI3 <62484669+AHUITZOTL3@users.noreply.github.com>
2022-03-04 16:57:54 -0600

    Add files via upload

--- validarm.php
+++ validarm.php
@@ -1,5 +1,4 @@
 <?php
-
 //include('conexion.php');
 //$conexion=mysqli_connect("localhost","root","","mercado");
 /*$nombre=$_POST['nombre'];*/
@@ -24,7 +23,7 @@ if(
 
     isset($_POST["coordenas"]) 
 ){
-    $mysqli->query($conexion, "INSERT INTO ubicacion(Calle,Numint,Numext,Colonia,Ciudad,Estado) VALUES(
+    $conexion->query("INSERT INTO ubicacion(Calle,Numint,Numext,Colonia,Ciudad,Estado) VALUES(
             '".$_POST["calle"]."',
             '".$_POST["numi"]."',
             '".$_POST["nume"]."',
@@ -32,30 +31,33 @@ if(
             '".$_POST["ciudad"]."',
             '".$_POST["estado"]."'
         )
-        ");
+    ");
 
 
-    $mysqli->query($conexion, "INSERT INTO usuarios(Usuario,Contraseña,id_rol,id_pro,id_mercado,id_certificado) VALUES(
-        '".$_POST["usuario"]."',
-        '".$_POST["contraseña"]."',
-        '3',
-        ".$mysqli->insert_id.",
-        ".$mysqli->insert_id.",
-        ".$mysqli->insert_id.") 
-        ");
-
-    $mysqli->query($conexion, "INSERT INTO mercadoorganico(Nombremercado,pancarta,id_ubicacion) VALUES(
+    $conexion->query("INSERT INTO mercadoorganico(Nombremercado,pancarta,id_ubicacion) VALUES(
         '".$_POST["nombre"]."',
         '".$_POST["aapellido"]."',
         '".$_POST["edad"]."',
         '".$_POST["email"]."',
-        ".$mysqli->insert_id.") 
-        ");
+        ".$conexion->insert_id."
+        ) 
+    ");
       
-    $mysqli->query($conexion, "INSERT INTO grafico(Ubicacion, idubicacionn) VALUES(
+    $conexion->query("INSERT INTO grafico(Ubicacion, idubicacionn) VALUES(
         '".$_POST["coordenas"]."',
-        ".$mysqli->insert_id.") 
-          ");
+        ".$conexion->insert_id."
+        ) 
+    ");
+    
+    $conexion->query("INSERT INTO usuarios(Usuario,Contraseña,id_rol,id_pro,id_mercado,id_certificado) VALUES(
+        '".$_POST["usuario"]."',
+        '".$_POST["contraseña"]."',
+        '3',
+        ".$conexion->insert_id.",
+        ".$conexion->insert_id.",
+        ".$conexion->insert_id."
+        ) 
+    ");
 
         header("Location: index.php");
 }
